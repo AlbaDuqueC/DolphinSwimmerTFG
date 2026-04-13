8306e036b8fd2e51723af2e64ef09d7d74dafa0f
AlbaDuqueC <alba.duque@iesnervion.es>
2026-04-13 13:57:09 +0200

    quitar la app del repositorio

--- SwimmingApp/SwimmingApp/app/src/main/java/com/swimming/app/SwimmingApp.kt
+++
@@ -1,8 +0,0 @@
-package com.swimming.app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-/** Clase principal de la aplicación. Necesaria para que Hilt funcione. */
-@HiltAndroidApp
-class SwimmingApp : Application()
